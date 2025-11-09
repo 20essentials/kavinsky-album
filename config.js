1cb1341908ee854073265481d400b7dee8e3c1a6
DominicDev <s0l0estudi4r@gmail.com>
2025-11-09 22:06:40 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'KAVINSKY';
+export let nameSongs = [
+	"NIGHTCALL", /*1*/
+	"PACIFIC COAST HIGHWAY", /*2*/
+	"LONDON GRAMMAR - NIGHTCALL", /*3*/
+	"ODD LOOK", /*4*/
+	"ROADGAME", /*5*/
+	"FIRST BLOOD", /*6*/
+	"OUTSIDER", /*7*/
+	"HORIZON", /*8*/
+	"PLASMA", /*9*/
+	"CAMEO", /*10*/
+	"ZENITH", /*11*/
+	"GOODBYE", /*12*/
+	"PULSAR", /*13*/
+	"TRIGGER", /*14*/
+	"ENDLESS", /*15*/
+	"BLIZZARD", /*16*/
+	"GRAND CANYON", /*17*/
+	"THE CRASH", /*18*/
+	"GHOST TRANSISTOR", /*19*/
+	"WAYFARER", /*20*/
+]
